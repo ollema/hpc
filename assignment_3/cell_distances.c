fbf13c0903844fed444762f1b1be275c494b9674
Olle Månsson <31876997+ollema@users.noreply.github.com>
2019-10-14 13:58:42 +0200

    initial commit for assignment 3

---
+++ assignment_3/cell_distances.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <complex.h>
+
+// arguments
+int threads = -1;
+
+int main(int argc, char **argv)
+{
+    // ########################################################################
+    // parse arguments
+    // ########################################################################
+    extern char *optarg;
+    extern int optind;
+    int c, err = 0;
+
+    static char usage[] = "\nusage: %s -t threads\n";
+
+    while ((c = getopt(argc, argv, "t:")) != -1)
+    {
+        switch (c)
+        {
+        case 't':
+            threads = atoi(optarg);
+            break;
+        case '?':
+            err = 1;
+            break;
+        }
+    }
+
+    if (threads == -1)
+    {
+        printf("error: -t is required!\n");
+        fprintf(stderr, usage, argv[0]);
+        exit(1);
+    }
+    if (err)
+    {
+        fprintf(stderr, usage, argv[0]);
+        exit(1);
+    }
+
+    if (threads < 1)
+    {
+        printf("error: t should be an positive integer!\n");
+        fprintf(stderr, usage, argv[0]);
+        exit(1);
+    }
+    // argument parsing done!
+
+    // ########################################################################
+    // computation part below
+    // ########################################################################
+}
