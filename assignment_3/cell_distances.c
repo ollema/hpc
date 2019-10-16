be1fbdcaba75c4c5496cb2c9deba345d17efa3a7
Olle Månsson <31876997+ollema@users.noreply.github.com>
2019-10-16 15:29:58 +0200

    add read logic

--- assignment_3/cell_distances.c
+++ assignment_3/cell_distances.c
@@ -4,13 +4,19 @@
 
 // arguments
 int threads = -1;
+int number_of_coords = 0;
+int block_size;
 
-int coord_to_int(const char *p)
+int *coords_1;
+int *coords_2;
+
+int distances[34642];
+
+int raw_coord_to_int(const char *p)
 {
     int x = 0;
     char negative = 0;
 
-    // handle sign
     if (*p == '-')
     {
         negative = 1;
@@ -33,6 +39,11 @@ int coord_to_int(const char *p)
         x = -x;
     }
     return x;
+}
+
+void compute_distance(int i, int j, int k, int l)
+{
+    distances[9999 + k + l] += 1;
 }
 
 int main(int argc, char **argv)
@@ -82,13 +93,11 @@ int main(int argc, char **argv)
     // ########################################################################
     // computation part below
     // ########################################################################
-
-    int block_size = 5;
-    int number_of_coords = 0;
     char ch;
     FILE *input_file;
 
-    input_file = fopen("/home/hpc2019/a3_grading/test_data/cell_50", "r");
+    // input_file = fopen("/home/hpc2019/a3_grading/test_data/cell_50", "r");
+    input_file = fopen("/home/hpcuser004/assignment_3/test_input", "r");
 
     while (!feof(input_file))
     {
@@ -99,21 +108,17 @@ int main(int argc, char **argv)
         }
     }
 
+    // TODO: increase
+    block_size = 5;
+
     char *raw_coords_1 = malloc(sizeof raw_coords_1 * block_size * 24);
     char *raw_coords_2 = malloc(sizeof raw_coords_2 * block_size * 24);
-
-    int *coords_entries_1 = malloc(sizeof coords_entries_1 * block_size * 3);
-    int **coords_1 = malloc(sizeof coords_1 * block_size);
-    for (size_t i = 0, j = 0; i < block_size; ++i, j += 3)
-        coords_1[i] = coords_entries_1 + j;
-
-    int *coords_entries_2 = malloc(sizeof coords_entries_2 * block_size * 3);
-    int **coords_2 = malloc(sizeof coords_2 * block_size);
-    for (size_t i = 0, j = 0; i < block_size; ++i, j += 3)
-        coords_2[i] = coords_entries_2 + j;
+    coords_1 = malloc(sizeof coords_1 * block_size * 3);
+    coords_2 = malloc(sizeof coords_1 * block_size * 3);
 
     int i = 0, j = 0;
     int k = 0, l = 0;
+    int coord;
 
     for (i = 0; i < number_of_coords; i += block_size)
     {
@@ -125,16 +130,66 @@ int main(int argc, char **argv)
             fseek(input_file, j * 24, SEEK_SET);
             fread(raw_coords_2, sizeof(char), 24 * block_size, input_file);
 
+            for (coord = 0; coord < block_size * 3; coord += 3)
+            {
+                coords_1[coord] = raw_coord_to_int(raw_coords_1 + 8 * coord);
+                coords_1[coord + 1] = raw_coord_to_int(raw_coords_1 + 8 + 8 * coord);
+                coords_1[coord + 2] = raw_coord_to_int(raw_coords_1 + 16 + 8 * coord);
+
+                coords_2[coord] = raw_coord_to_int(raw_coords_2 + 8 * coord);
+                coords_2[coord + 1] = raw_coord_to_int(raw_coords_2 + 8 + 8 * coord);
+                coords_2[coord + 2] = raw_coord_to_int(raw_coords_2 + 16 + 8 * coord);
+            }
+
+            // if (i == 0 && j == 5)
+            // {
+            //     for (k = 0; k < block_size; k++)
+            //     {
+            //         for (l = 0; l < 3; l++)
+            //         {
+            //             printf("%d ", coords_1[k * 3 + l]);
+            //         }
+            //         printf("\n");
+            //     }
+
+            //     printf("\n");
+
+            //     for (k = 0; k < block_size; k++)
+            //     {
+            //         for (l = 0; l < 3; l++)
+            //         {
+            //             printf("%d ", coords_2[k * 3 + l]);
+            //         }
+            //         printf("\n");
+            //     }
+
+            //     exit(0);
+            // }
+
             for (k = 0; k < block_size; k++)
             {
                 for (l = 0; l < block_size; l++)
                 {
-                    // compute_distance(k, l, i, j);
+                    compute_distance(i, j, k, l);
                 }
-
             }
+        }
+    }
 
+    int distance;
+    int number;
+    int rest;
 
+    for (int i = 0; i < 34642; i++)
+    {
+        distance = distances[i];
+
+        if (distance != 0)
+        {
+            number = i / 1000;
+            rest = i % 100;
+            // printf("%d: %d\n", i, distance);
+            printf("%02d.%02d: %d\n", number, rest, distance);
         }
     }
 }
