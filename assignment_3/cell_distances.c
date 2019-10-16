a19992e091edd94134963aa7ac5b0e3e841bfaa9
Olle Månsson <31876997+ollema@users.noreply.github.com>
2019-10-16 17:18:38 +0200

    separate compute_distance

--- assignment_3/cell_distances.c
+++ assignment_3/cell_distances.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <math.h>
+#include <time.h>
 
 // arguments
 int threads = -1;
@@ -44,20 +45,15 @@ int raw_coord_to_int(const char *p)
 
 void compute_distance(int i, int j, int k, int l)
 {
-    int global_position_row = i + k;
-    int global_position_column = j + l;
-    if (global_position_row < global_position_column)
-    {
-        // Calculate distances
-        int diff_1 = coords_1[3 * l] - coords_2[3 * k];
-        int diff_2 = coords_1[3 * l + 1] - coords_2[3 * k + 1];
-        int diff_3 = coords_1[3 * l + 2] - coords_2[3 * k + 2];
+    // Calculate distances
+    int diff_1 = coords_1[3 * l] - coords_2[3 * k];
+    int diff_2 = coords_1[3 * l + 1] - coords_2[3 * k + 1];
+    int diff_3 = coords_1[3 * l + 2] - coords_2[3 * k + 2];
 
-        float temp = sqrt(diff_1 * diff_1 + diff_2 * diff_2 + diff_3 * diff_3);
-        int distance = round(temp/10);
+    float temp = sqrt(diff_1 * diff_1 + diff_2 * diff_2 + diff_3 * diff_3);
+    int distance = round(temp * 0.1);
 
-        distances[distance] += 1;
-    }
+    distances[distance] += 1;
 }
 
 int main(int argc, char **argv)
@@ -65,6 +61,13 @@ int main(int argc, char **argv)
     // ########################################################################
     // parse arguments
     // ########################################################################
+    struct timespec initial_time, final_time;
+    int delta_time_s;
+    long double delta_time_ns;
+    long double delta_time;
+
+    timespec_get(&initial_time, TIME_UTC);
+
     extern char *optarg;
     extern int optind;
     int c, err = 0;
@@ -157,13 +160,34 @@ int main(int argc, char **argv)
                 coords_2[coord + 2] = raw_coord_to_int(raw_coords_2 + 16 + 8 * coord);
             }
 
+            timespec_get(&final_time, TIME_UTC);
+
+            delta_time_s = final_time.tv_sec - initial_time.tv_sec;
+            delta_time_ns = final_time.tv_nsec - initial_time.tv_nsec;
+
+            delta_time = delta_time_s + delta_time_ns / 1000000000;
+            printf("Pre time elapsed: \t\t\t%.9Lfs\n", delta_time);
+
             for (k = 0; k < block_size; k++)
             {
                 for (l = 0; l < block_size; l++)
                 {
-                    compute_distance(i, j, k, l);
+                    int global_position_row = i + k;
+                    int global_position_column = j + l;
+                    if (global_position_row < global_position_column)
+                    {
+                        compute_distance(i, j, k, l);
+                    }
                 }
             }
+
+            timespec_get(&final_time, TIME_UTC);
+
+            delta_time_s = final_time.tv_sec - initial_time.tv_sec;
+            delta_time_ns = final_time.tv_nsec - initial_time.tv_nsec;
+
+            delta_time = delta_time_s + delta_time_ns / 1000000000;
+            printf("After time elapsed: \t\t\t%.9Lfs\n", delta_time);
         }
     }
 
@@ -178,7 +202,7 @@ int main(int argc, char **argv)
         {
             number = i / 100;
             rest = i % 100;
-            printf("%02d.%02d %d\n", number, rest, distance);
+            // printf("%02d.%02d %d\n", number, rest, distance);
         }
     }
 }
