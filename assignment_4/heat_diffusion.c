47bf4f8b50639307d21f385c7497005514093f00
Olle Månsson <31876997+ollema@users.noreply.github.com>
2019-10-25 11:41:26 +0200

    add TODO comments

--- assignment_4/heat_diffusion.c
+++ assignment_4/heat_diffusion.c
@@ -77,6 +77,7 @@ int main(int argc, char **argv)
     // computation part below
     // ########################################################################
 
+    // TODO: determine if we need two matrices - one for current state vs. next state etc.
     double *temperature_entries = malloc(sizeof(double) * width * height);
     temperatures = malloc(sizeof(double *) * height);
     for (size_t i = 0, j = 0; i < height; ++i, j += width)
@@ -85,8 +86,9 @@ int main(int argc, char **argv)
     for (size_t i = 0; i < width; ++i)
         for (size_t j = 0; j < height; ++j)
             temperatures[i][j] = 0.0;
+    // end of determine if we need two matrices - one for current state vs. next state etc.
 
-    // remove soon
+    // TODO: remove and replace with our own above
     const int LIST_SIZE = 1024;
     int *A = (int *)malloc(sizeof(int) * LIST_SIZE);
     int *B = (int *)malloc(sizeof(int) * LIST_SIZE);
@@ -95,6 +97,7 @@ int main(int argc, char **argv)
         A[i] = i;
         B[i] = LIST_SIZE - i;
     }
+    // end of remove and replace with our own above
 
     FILE *fp;
     char *source_str;
@@ -111,6 +114,7 @@ int main(int argc, char **argv)
     fclose(fp);
 
     // Get platform and device information
+    // DONE!
     cl_platform_id platform_id = NULL;
     cl_device_id device_id = NULL;
     cl_uint ret_num_devices;
@@ -120,56 +124,72 @@ int main(int argc, char **argv)
                          &device_id, &ret_num_devices);
 
     // Create an OpenCL context
+    // DONE!
     cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 
     // Create a command queue
+    // DONE!
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
     // Create memory buffers on the device for each vector
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-                                      LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-                                      LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-                                      LIST_SIZE * sizeof(int), NULL, &ret);
+    // TODO: use correct size for buffers here
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(int), NULL, &ret);
+    // end of use correct size for buffers here
 
     // Copy the lists A and B to their respective memory buffers
-    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-                               LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0,
-                               LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
+    // TODO: use our own matrices here
+    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
+    // end of use our own matrices here
 
     // Create a program from the kernel source
-    cl_program program = clCreateProgramWithSource(context, 1,
-                                                   (const char **)&source_str, (const size_t *)&source_size, &ret);
+    // DONE!
+    cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
 
     // Build the program
+    // DONE!
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
     // Create the OpenCL kernel
+    // TODO: rename string? not sure if more is needed
     cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+    //end of rename string? not sure if more is needed
+
 
     // Set the arguments of the kernel
+    // TODO: use our own args here
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
     ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
     ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
+    // end of use our own args here
+
 
     // Execute the OpenCL kernel on the list
+    // TODO: TBD
     size_t global_item_size = LIST_SIZE; // Process the entire lists
     size_t local_item_size = 64;         // Process in groups of 64
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-                                 &global_item_size, &local_item_size, 0, NULL, NULL);
+    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, NULL);
+    // end of TBD
+
 
     // Read the memory buffer C on the device to the local variable C
+    // TODO: read our own results
     int *C = (int *)malloc(sizeof(int) * LIST_SIZE);
-    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0,
-                              LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+    // end of read our own results
+
 
     // Display the result to the screen
+    // TODO: output our results
     for (i = 0; i < LIST_SIZE; i++)
         printf("%d + %d = %d\n", A[i], B[i], C[i]);
+    // end of output our results
+
 
     // Clean up
+    // TODO: clean up our variables
     ret = clFlush(command_queue);
     ret = clFinish(command_queue);
     ret = clReleaseKernel(kernel);
@@ -182,5 +202,7 @@ int main(int argc, char **argv)
     free(A);
     free(B);
     free(C);
+    // end of clean up our variables
+
     return 0;
 }
